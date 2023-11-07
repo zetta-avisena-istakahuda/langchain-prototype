6ad11b87e22448eade548e8f1f23129c2b0ed624
zetta-avisena-istakahuda <121282835+zetta-avisena-istakahuda@users.noreply.github.com>
2023-11-07 19:03:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import pinecone
 import os
 
+pinecone.init(api_key='bbb687a2-cfb9-4b3e-8210-bece030f2776', environment='gcp-starter')
+
 chat_history = []
-isVectorStore = false
 # Function to generate answers based on questions
 def generate_answer(question):
     # Replace this with your logic to generate answers
@@ -23,13 +25,11 @@ def insert_or_fetch_embeddings(index_name):
   openai_api_key = api_config["openai_api_key"]
 
   embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-  pinecone.init(api_key='bbb687a2-cfb9-4b3e-8210-bece030f2776', environment='gcp-starter')
 
   if index_name in pinecone.list_indexes():
    print(f'Index {index_name} already exists. Loading embeddings ... ', end='')
    vector_store = Pinecone.from_existing_index(index_name, embeddings)
    print('OK')
-   isVectorStore = true
   return vector_store
 
 
@@ -67,13 +67,11 @@ def ask_with_memory(vector_store, question, chat_history=[]):
 def main():
     from dotenv import load_dotenv
     global chat_history
-    global isVectorStore
     api_config = st.secrets["api"]
     openai_api_key = api_config["openai_api_key"]
     load_dotenv()
     index_name = 'demo-langchain'
-    if isVectorStore:
-      vector_store = insert_or_fetch_embeddings(index_name)
+    vector_store = insert_or_fetch_embeddings(index_name)
 
     # Create a layout with two columns
     left_column, right_column = st.columns([1, 3])
