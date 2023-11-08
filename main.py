068f36c3b50c1a5f02bb93c368fd37cdb621e1ca
zetta-avisena-istakahuda <121282835+zetta-avisena-istakahuda@users.noreply.github.com>
2023-11-08 11:10:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def ask_and_get_answer(vector_store, query):
   api_config = st.secrets["api"]
   openai_api_key = api_config["openai_api_key"]
   
-  llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.3, openai_api_key=openai_api_key)
+  llm = ChatOpenAI(model='gpt-3.5-turbo-0301', temperature=0.3, openai_api_key=openai_api_key)
   retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k':3})
   chain=RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
   answer = chain.run(query)
