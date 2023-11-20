a0f2fbb2685825fc293e93f486f8d893634754f2
zetta-avisena-istakahuda <121282835+zetta-avisena-istakahuda@users.noreply.github.com>
2023-11-20 16:02:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -232,9 +232,9 @@ def main():
 
     # Input field for the question
     with right_column:
-        st.title("CPEB 2024 - Posez-nous vos questions...")
-        question = st.text_input(f"Entrez votre question: ")
-        if st.button("Obtenir la réponse") or question:
+        st.title("FE DEV GUIDELINES - Ask me questions...")
+        question = st.text_input(f"Enter question: ")
+        if st.button("Get Answer") or question:
             # Check if a question is provided
             if not question:
                 st.warning("Please enter a question.")
