2308918aeb8583b550f44b65a146bcc61aa450a1
zetta-avisena-istakahuda <121282835+zetta-avisena-istakahuda@users.noreply.github.com>
2023-11-21 14:49:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def initRAG(vector_store):
   openai_api_key = api_config["openai_api_key"]  
   
   llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.15, max_tokens=512, openai_api_key=openai_api_key)
-  retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k':3})
+  retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k':1})
   condense_q_system_prompt = """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. Always answer in language user asks. If the answer is long, try to make it to be bullet points."""
   condense_q_prompt = ChatPromptTemplate.from_messages(
     [
