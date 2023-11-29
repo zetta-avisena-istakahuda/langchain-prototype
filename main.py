d749346e56cedb9251395157ce8ddd35faf7443e
zetta-avisena-istakahuda <121282835+zetta-avisena-istakahuda@users.noreply.github.com>
2023-11-29 14:08:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def initRAG(vector_store):
   
   llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.15, max_tokens=512, openai_api_key=openai_api_key)
   retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k':1})
-  condense_q_system_prompt = """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. Always answer in language user asks. If the answer is long, try to make it to be bullet points.
+  condense_q_system_prompt = """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know, say you don't know. Always answer in language user asks. If the answer is long, try to make it to be bullet points.
    When creating multiple choices quiz, set the 4 choices in bullet points with only ONE right answer and put the right answer below it with explanation. Example:
    What is the purpose of the document "General Standardization Development Guideline"?
    A) Translation 
@@ -50,7 +50,7 @@ def initRAG(vector_store):
   )
   condense_q_chain = condense_q_prompt | llm | StrOutputParser()
   qa_system_prompt = """
-  You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. Always answer in language user asks. If the answer is long, try to make it to be bullet points.
+  You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know, say you don't know. Always answer in language user asks. If the answer is long, try to make it to be bullet points.
    When creating multiple choices quiz, set the 4 choices in bullet points with only ONE right answer and put the right answer below it with explanation. Example:
    What is the purpose of the document "General Standardization Development Guideline"?
    A) Translation 
