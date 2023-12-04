de59e39f4798314c65b22bd75ddfbf6cbaf4a16e
zetta-avisena-istakahuda <121282835+zetta-avisena-istakahuda@users.noreply.github.com>
2023-12-04 19:52:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -105,8 +105,8 @@ def detect_and_create_quizzes(text, chat_history=[]):
           result_container.markdown(f" {formatted_content}", unsafe_allow_html=True
          # st.session_state.chat_history.extend([HumanMessage(content=question), formatted_content])
          # original_number -= number_of_quiz_per_iteration
-    else:
-      return False
+    # else:
+    #   return False
 
 def insert_or_fetch_embeddings(index_name):
   global isVector
