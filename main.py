f60090ba4c6629b9842c44468c72116d1678eab8
zetta-avisena-istakahuda <121282835+zetta-avisena-istakahuda@users.noreply.github.com>
2023-12-04 19:59:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -96,26 +96,19 @@ def detect_and_create_quizzes(text, chat_history=[]):
             time.sleep(1)
             if isFirst:
              question = re.sub(r'\b\d+\b', str(min(original_number, number_of_quiz_per_iteration)), text)
-                ai_msg = rag_chain.invoke({"question": question, "chat_history": chat_history})
-                # for chunk in ai_msg:
-                #  print(chunk.content, end="", flush=True)
-                #  ai_msg_early.content += chunk.content
-                formatted_content = ai_msg.content.replace('\n', '<br>')
-                result_container.markdown(f"{formatted_content}", unsafe_allow_html=True)
+             ai_msg = rag_chain.invoke({"question": question, "chat_history": chat_history})
+             formatted_content = ai_msg.content.replace('\n', '<br>')
+             result_container.markdown(f"{formatted_content}", unsafe_allow_html=True)
              isFirst = False
             else:
              question = f"Continue the number. Don't jump the number. Create {min(original_number, number_of_quiz_per_iteration)} again different quizzes"
              try:
                 ai_msg = rag_chain.invoke({"question": question, "chat_history": chat_history})
-                # for chunk in ai_msg:
-                #  print(chunk.content, end="", flush=True)
-                #  ai_msg_early.content += chunk.content
                 formatted_content = ai_msg.content.replace('\n', '<br>')
                 result_container.markdown(f"{formatted_content}", unsafe_allow_html=True)
              except Exception as e:
               print(f"An error occurred: {e}")
-            chat_history.extend([HumanMessage(content=question), ai_msg_early])
-            print("")
+            st.session_state.chat_history.extend([HumanMessage(content=question), ai_msg_early])
             original_number -= number_of_quiz_per_iteration
     else:
        return False
