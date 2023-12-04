22a3d8365227ea9410774d0630852ded3e5c6a0e
zetta-avisena-istakahuda <121282835+zetta-avisena-istakahuda@users.noreply.github.com>
2023-12-04 19:30:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -94,20 +94,16 @@ def detect_and_create_quizzes(text, chat_history=[]):
         while original_number > 0:
             time.sleep(1)
             if isFirst:
-             question = re.sub(r'\b\d+\b', str(min(original_number, number_of_quiz_per_iteration)), text)
-             ai_msg = rag_chain.invoke({"question": question, "chat_history": chat_history})
-             # for chunk in ai_msg:
-             #   ai_msg_early.content += chunk.content
-             formatted_content = ai_msg.content.replace('\n', '<br>')
-             #   st.write(f"Answer: {formatted_content}")
-             #   result_container.markdown(f" **Answer:** {formatted_content}", unsafe_allow_html=True)
-             result_container.markdown(f" {formatted_content}", unsafe_allow_html=True)
-             isFirst = False
+              question = re.sub(r'\b\d+\b', str(min(original_number, number_of_quiz_per_iteration)), text)
+              ai_msg = rag_chain.invoke({"question": question, "chat_history": chat_history})
+              formatted_content = ai_msg.content.replace('\n', '<br>')
+              result_container.markdown(f" {formatted_content}", unsafe_allow_html=True)
+              isFirst = False
             else:
-             question = f"Continue the number. Don't jump the number. Create {min(original_number, number_of_quiz_per_iteration)} again different quizzes"
-             ai_msg = rag_chain.invoke({"question": question, "chat_history": chat_history})
-             formatted_content = ai_msg_early.content.replace('\n', '<br>')
-             result_container.markdown(f" {formatted_content}", unsafe_allow_html=True
+              question = f"Continue the number. Don't jump the number. Create {min(original_number, number_of_quiz_per_iteration)} again different quizzes"
+              ai_msg = rag_chain.invoke({"question": question, "chat_history": chat_history})
+              formatted_content = ai_msg_early.content.replace('\n', '<br>')
+              result_container.markdown(f" {formatted_content}", unsafe_allow_html=True
             st.session_state.chat_history.extend([HumanMessage(content=question), ai_msg_early])
             original_number -= number_of_quiz_per_iteration
     else:
