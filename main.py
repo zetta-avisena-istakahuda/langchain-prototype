cd2033678bed5892114fb431c6af3c4e51a9000c
zetta-avisena-istakahuda <121282835+zetta-avisena-istakahuda@users.noreply.github.com>
2023-12-06 17:18:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def initRAG(vector_store):
   openai_api_key = api_config["openai_api_key"]  
   
   llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0, max_tokens=512, openai_api_key=openai_api_key)
-  retriever = vstore.as_retriever(search_type='similarity_score_threshold', search_kwargs={"score_threshold": .7}, filters={'metadata': {'source': 'emarketing_textbook_downoad'}})  
+  retriever = vector_store.as_retriever(search_type='similarity_score_threshold', search_kwargs={"score_threshold": .7}, filters={'metadata': {'source': 'emarketing_textbook_downoad'}})  
   condense_q_system_prompt = """
   - answer only if the question is relevant to the document
   - if asked to create quiz/question, say that you are not allowed to help
