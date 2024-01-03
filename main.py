c7fc9b80aa29199a1a9db9d815e399708690f896
zetta-avisena-istakahuda <121282835+zetta-avisena-istakahuda@users.noreply.github.com>
2024-01-03 16:25:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ vector_store = None
 question = None
 
 def check_similarity(answer, chunks):
-
+ nlp = spacy.load("en_core_web_sm")
  answer_tokens = nlp(answer)
  chunks_tokens = nlp(chunks)
  similarity_score = calculate_similarity(answer_tokens.text, chunks_tokens.text)
